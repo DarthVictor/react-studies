d3f6f5f86e2d66492ea45313dbe04d1a8e029133
Victor <follet.victor@gmail.com>
2016-10-11 18:30:25 +0300

    Chapter 10.0 EventEmitter installation

--- tsc-webpack-react-course-ru/src/components/App.tsx
+++ tsc-webpack-react-course-ru/src/components/App.tsx
@@ -3,34 +3,49 @@ import {ArticleData} from './Article';
 import {News, NewsProps} from './News';
 import {Add} from './Add';
 
-//export interface HelloProps { compiler: string; framework: string; }
+export interface AppState { my_news:  ArticleData[]}
 
-export class App extends React.Component<{}, {}> {
+export class App extends React.Component<{}, AppState> {
+    constructor(){
+        super();
+        this.state = {
+            my_news:  [
+                        {
+                            author: 'Саша Печкин',
+                            text: 'В четчерг, четвертого числа...',
+                            bigText: 'в четыре с четвертью часа четыре чёрненьких чумазеньких чертёнка чертили чёрными чернилами чертёж.'
+                        },
+                        {
+                            author: 'Просто Вася',
+                            text: 'Считаю, что $ должен стоить 35 рублей!',
+                            bigText: 'А евро 42!'
+                        },
+                        {
+                            author: 'Гость',
+                            text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000',
+                            bigText: 'На самом деле платно, просто нужно прочитать очень длинное лицензионное соглашение'
+                        }
+                    ] 
+        };
+    }
     
+    componentDidMount() {
+        /* Слушай событие "Создана новость"  если событие произошло, обнови this.state.news */
+
+    }
+    
+    componentWillUnmount() {
+        /* Больше не слушай событие "Создана новость" */
+
+    }
+
     render() {
-        const my_news: ArticleData[] = [
-            {
-                author: 'Саша Печкин',
-                text: 'В четчерг, четвертого числа...',
-                bigText: 'в четыре с четвертью часа четыре чёрненьких чумазеньких чертёнка чертили чёрными чернилами чертёж.'
-            },
-            {
-                author: 'Просто Вася',
-                text: 'Считаю, что $ должен стоить 35 рублей!',
-                bigText: 'А евро 42!'
-            },
-            {
-                author: 'Гость',
-                text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000',
-                bigText: 'На самом деле платно, просто нужно прочитать очень длинное лицензионное соглашение'
-            }
-        ]
+        
         return (
             <div className="app">
                 <h3>Новости</h3>
                 <Add />
-                <News data={my_news} /> {/*добавили свойство data */}
-                {/*<Comments /> */}
+                <News data={this.state.my_news} />   
             </div>
         );
     }
