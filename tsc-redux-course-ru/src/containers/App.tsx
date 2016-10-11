55ebd2381bd2bf3259d6b96520f328cc02cb4a8c
Victor Follet <follet.victor@gmail.com>
2016-10-12 02:15:51 +0300

    tsc-redux-course-ru: first steps

---
+++ tsc-redux-course-ru/src/containers/App.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+export class App extends  React.Component<{}, {}> {
+    render() {
+        return <div>Привет из App 2</div>
+    }
+}
---
+++ tsc-redux-course-ru/src/index.tsx
@@ -0,0 +1,19 @@
+// import * as React from "react";
+// import * as ReactDOM from "react-dom";
+
+// import { Hello } from "./components/Hello";
+
+// ReactDOM.render(
+//     <Hello compiler="TypeScript" framework="React" />,
+//     document.getElementById("example")
+// );
+
+//import 'babel-polyfill'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import App from './containers/App'
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
