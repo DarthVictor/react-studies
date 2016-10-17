0c70136c9bc514d39ac0294ed39d4d177f108b43
Victor <follet.victor@gmail.com>
2016-10-17 20:08:38 +0300

    Update vk page actions

--- tsc-redux-course-ru/src/actions/PageActions.ts
+++ tsc-redux-course-ru/src/actions/PageActions.ts
@@ -1,8 +1,53 @@
 import * as Redux from 'redux' 
 import {Action, actionCreator} from './ActionTypes' 
-import {GET_PHOTOS_REQUEST, GET_PHOTOS_SUCCESS} from '../constants/Page'
+import {GET_PHOTOS_REQUEST, GET_PHOTOS_FAIL, GET_PHOTOS_SUCCESS} from '../constants/Page'
 import {Year} from '../types' 
   
+declare var VK: any;
+
+let photosArr: any[] = []
+let cached = false
+
+function makeYearPhotos(photos: any[], selectedYear: number) {
+    let createdYear: number, 
+        yearPhotos: any[] = []
+    photos.forEach((item: any) => {
+        createdYear = new Date(item.created*1000).getFullYear()
+        if (createdYear === selectedYear ) {
+            yearPhotos.push(item)
+        }
+    })
+    yearPhotos.sort((a,b) => b.likes.count - a.likes.count);
+    return yearPhotos
+}
+function getMorePhotos(offset: number, count: number, year: number, dispatch: Redux.Dispatch<any>) {
+    VK.Api.call('photos.getAll', {extended:1, count: count, offset: offset}, (r: any ) => {
+        try {
+            if (offset <= r.response[0] - count) {
+                offset += 200;
+                photosArr = photosArr.concat(r.response)
+                getMorePhotos(offset, count, year, dispatch)
+            } 
+            else {
+            let photos = makeYearPhotos(photosArr, year)
+                cached = true
+                dispatch({
+                    type: GET_PHOTOS_SUCCESS,
+                    payload: photos
+                })
+            }
+        }
+        catch(e) {
+            dispatch({
+                type: GET_PHOTOS_FAIL,
+                error: true,
+                payload: new Error(e)
+                })
+        }
+    })
+}
+
+
 export interface PageActionsProps {
     getPhotos(year: Year) : (dispatch: Redux.Dispatch<any>) => any
 }
