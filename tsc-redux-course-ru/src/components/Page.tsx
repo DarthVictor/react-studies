365a7f63ab24b73ee721932d121184081b386b7a
Victor Follet <follet.victor@gmail.com>
2016-10-18 01:02:02 +0300

    VK chapter finished

--- tsc-redux-course-ru/src/components/Page.tsx
+++ tsc-redux-course-ru/src/components/Page.tsx
@@ -8,22 +8,28 @@ export default class Page extends React.Component<PageProps, {}> {
     onYearBtnClick(e: Event) {
         this.props.getPhotos(Number( (e.target as HTMLElement).textContent) )
     }
+    componentWillMount(){
+        this.props.getPhotos(this.props.year )
+    }
     render() {
-        const { year, photos, fetching } = this.props;
+        const { year, photos, fetching, error } = this.props;
         const onYearBtnClick = this.onYearBtnClick.bind(this)
+        const years = [2016,2015,2014,2013,2012,2011,2010]
+
         return <div className='ib page'>
-            <p>
-            <button className='btn' onClick={onYearBtnClick}>2016</button>{' '}
-            <button className='btn' onClick={onYearBtnClick}>2015</button>{' '}
-            <button className='btn' onClick={onYearBtnClick}>2014</button>
-            </p>
-            <h3>{year} год</h3>
-            {
-                fetching ?
-                    <p>Загрузка...</p>
-                :
-                    <p>У тебя {photos.length} фото.</p>
-            }
+                    <p>
+                        { years.map((item,index) => <button className='btn' key={index} onClick={onYearBtnClick}>{item}</button> )}
+                    </p>
+                    <h3>{year} год [{photos.length}]</h3>
+                { error ? <p className='error'> Во время загрузки фото произошла ошибка</p> : ''  }
+                { fetching ?  <p>Загрузка...</p>
+                           :  photos.map((entry: any, index: number) =>
+                                    <div key={index} className='photo'>
+                                        <p><img src={entry.src} /></p>
+                                        <p>{entry.likes.count} ❤</p>
+                                    </div>
+                                )
+                }
             </div>
     }
 }
--- tsc-redux-course-ru/src/reducers/page.ts
+++ tsc-redux-course-ru/src/reducers/page.ts
@@ -1,20 +1,23 @@
 import {PageState, Year} from '../types'
-import {GET_PHOTOS_REQUEST, GET_PHOTOS_SUCCESS} from '../constants/Page'
+import {GET_PHOTOS_REQUEST, GET_PHOTOS_SUCCESS, GET_PHOTOS_FAIL} from '../constants/Page'
 import {Action, isType} from '../actions/ActionTypes'
 import {PageActions} from '../actions/PageActions'
 
 const initialState: PageState = {
-    year: 2016,
+    year: 2015,
     photos: [],
-    fetching: false
+    fetching: false,
+    error: ''
 }
 
 export default function userstate(state: PageState = initialState, action: Action<any>) {
     switch (action.type) {
         case GET_PHOTOS_REQUEST:
-            return Object.assign({}, state, {year: action.payload, fetching: true })
+            return Object.assign({}, state, {year: action.payload, fetching: true, error: '' })
         case GET_PHOTOS_SUCCESS:
-            return Object.assign({}, state, {photos: action.payload, fetching: false })
+            return Object.assign({}, state, {photos: action.payload, fetching: false, error: '' })
+        case GET_PHOTOS_FAIL:
+            return Object.assign({}, state, {error: action.payload.message, fetching: false })
         default:
             return state;
     }
