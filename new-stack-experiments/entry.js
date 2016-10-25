3574f4eef7306f20c34c4d83481a3e6d18a2e57b
Victor <follet.victor@gmail.com>
2016-10-25 18:25:01 +0300

    Webpack compiler call added to server.js

--- new-stack-experiments/entry.js
+++ new-stack-experiments/entry.js
@@ -1,1 +1,2 @@
+const t = 1;
 document.write('It works.');
---
+++ new-stack-experiments/server.js
@@ -0,0 +1,17 @@
+'use strict'
+const express = require('express')
+const config = require('./webpack.config')
+const webpack = require('webpack')
+const compiler = webpack(config)
+const util = require('util')
+
+compiler.run((err, data) => {
+    console.log(util.inspect(err, {showHidden: false, depth: null}))
+})
+
+compiler
+//const port = 3000
+//const app = express()
+// app.listen(port, () => {
+//     console.log('App running on port ' + port + '!')
+// })
