6c5677fa490588e4b25e594e35e6a9c2596cab2a
Victor Follet <follet.victor@gmail.com>
2016-11-06 18:15:06 +0300

    Server entry with HMR

--- new-stack-experiments/server.js
+++ new-stack-experiments/server.js
@@ -3,19 +3,18 @@ const express = require('express')
 const util = require('util')
 const path = require('path')
 
-//const config = require('./webpack.config')
-//const webpack = require('webpack')
-//const compiler = webpack(config)
-//const webpackDevMiddleware = require('webpack-dev-middleware')
-//const webpackHotMiddleware = require('webpack-hot-middleware')
+const config = require('./webpack.config')
+const webpack = require('webpack')
+const compiler = webpack(config)
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const webpackHotMiddleware = require('webpack-hot-middleware')
 const renderPath = require ('./dist/bundle.server').default
-console.log(renderPath)
 // Express init
 const app = express()
 
 //Webpack hot module reloading
-//app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
-//app.use(webpackHotMiddleware(compiler))
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
+app.use(webpackHotMiddleware(compiler))
 
 // Express run
 const port = 3000 
