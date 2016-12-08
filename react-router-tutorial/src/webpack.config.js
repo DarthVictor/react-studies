4596cdd46c63e04f184fcc6ad952e9468c9d1909
Victor Follet <follet.victor@gmail.com>
2016-12-09 01:43:11 +0300

    lesson-11 is done

--- react-router-tutorial/src/webpack.config.js
+++ react-router-tutorial/src/webpack.config.js
@@ -2,9 +2,16 @@ module.exports = {
   entry: './index.js',
 
   output: {
+    path: 'public',
     filename: 'bundle.js',
-    publicPath: ''
+    publicPath: '/'
   },
+  
+  plugins: process.env.NODE_ENV === 'production' ? [
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.OccurrenceOrderPlugin(),
+      new webpack.optimize.UglifyJsPlugin()
+    ] : [],
 
   module: {
     loaders: [
